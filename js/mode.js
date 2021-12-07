b46d07a0fa5b9cfcc6b59929d94b353f3558d7f8
xjtx <73387366+xiaoji235@users.noreply.github.com>
2021-12-07 22:05:24 +0800

    Add files via upload

--- js/mode.js
+++ js/mode.js
@@ -1,13 +1,15 @@
 mode=localStorage.getItem("mode")
 
 
-if(mode){
+if(mode=="night"){
 	document.getElementById('theme_css').href = 'css/night.css'
 }else{
 	document.getElementById('theme_css').href = 'css/day.css'
 }
 
 
+document.getElementById('nightbtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'css/night.css'; localStorage.setItem("mode","night")})
 
-document.getElementById('nightbtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'css/night.css'; })
-document.getElementById('daybtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'css/day.css'; })
+
+
+document.getElementById('daybtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'css/day.css'; localStorage.setItem("mode","day")})
