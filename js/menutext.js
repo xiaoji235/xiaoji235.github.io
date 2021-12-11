25ee0ff4103429a313ca6beabe2a47b997bf8592
xjtx <73387366+xiaoji235@users.noreply.github.com>
2021-12-11 11:02:03 +0800

    Update menutext.js

--- js/menutext.js
+++ js/menutext.js
@@ -1,42 +1,42 @@
 const searchEngines = [{
     "id": 0,
     "name": "百度",
-    "img": "url('search\/baidu.svg')",
+    "img": "url('https:\/\/cdn.jsdelivr.net\/gh\/xiaoji235\/Pure-Newtab\/search\/baidu.svg')",
     "url": "https:\/\/www.baidu.com\/s?wd="
 },
 {
     "id": 1,
     "name": "搜狗",
-    "img": "url('search\/sogou.svg')",
+    "img": "url('https:\/\/cdn.jsdelivr.net\/gh\/xiaoji235\/Pure-Newtab\/search\/sogou.svg')",
     "url": "https:\/\/www.sogou.com\/web?query="
 },
 {
     "id": 2,
     "name": "360",
-    "img": "url('search\/360.svg')",
+    "img": "url('https:\/\/cdn.jsdelivr.net\/gh\/xiaoji235\/Pure-Newtab\/search\/360.svg')",
     "url": "https:\/\/www.so.com\/s?q="
 },
 {
     "id": 3,
     "name": "Google",
-    "img": "url('search\/google.svg')",
+    "img": "url('https:\/\/cdn.jsdelivr.net\/gh\/xiaoji235\/Pure-Newtab\/search\/google.svg')",
     "url": "https:\/\/www.google.com\/search?q="
 },
 {
     "id": 4,
     "name": "Bing",
-    "img": "url('search\/bing.svg')",
+    "img": "url('https:\/\/cdn.jsdelivr.net\/gh\/xiaoji235\/Pure-Newtab\/search\/bing.svg')",
     "url": "https:\/\/cn.bing.com\/search?q="
 },
 {
     "id": 19,
     "name": "Github",
-    "img": "url('search\/github.svg')",
+    "img": "url('https:\/\/cdn.jsdelivr.net\/gh\/xiaoji235\/Pure-Newtab\/search\/github.svg')",
     "url": "https:\/\/github.com\/search?q="
 },
 {
     "id": 7,
     "name": "Yandex",
-    "img": "url('search\/yandex.svg')",
+    "img": "url('https:\/\/cdn.jsdelivr.net\/gh\/xiaoji235\/Pure-Newtab\/search\/yandex.svg')",
     "url": "https:\/\/yandex.com\/search\/?text="
 }];
