09055b15cf2d631e74697fe3bf9978f6db6a3f2d
xjtx <73387366+xiaoji235@users.noreply.github.com>
2021-12-11 11:18:58 +0800

    Update mode.js

--- js/mode.js
+++ js/mode.js
@@ -2,14 +2,14 @@ mode=localStorage.getItem("mode")
 
 
 if(mode=="night"){
-	document.getElementById('theme_css').href = 'css/night.css'
+	document.getElementById('theme_css').href = 'https://cdn.jsdelivr.net/gh/xiaoji235/xiaoji235.github.io/css/night.css'
 }else{
-	document.getElementById('theme_css').href = 'css/day.css'
+	document.getElementById('theme_css').href = 'https://cdn.jsdelivr.net/gh/xiaoji235/xiaoji235.github.io/css/day.css'
 }
 
 
-document.getElementById('nightbtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'css/night.css'; localStorage.setItem("mode","night")})
+document.getElementById('nightbtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'https://cdn.jsdelivr.net/gh/xiaoji235/xiaoji235.github.io/css/night.css'; localStorage.setItem("mode","night")})
 
 
 
-document.getElementById('daybtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'css/day.css'; localStorage.setItem("mode","day")})
+document.getElementById('daybtn').addEventListener('click', function(){ document.getElementById('theme_css').href = 'https://cdn.jsdelivr.net/gh/xiaoji235/xiaoji235.github.io/css/day.css'; localStorage.setItem("mode","day")})
