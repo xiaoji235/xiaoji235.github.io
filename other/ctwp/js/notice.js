67354cb1d9c673ccba6bf96c7fe3de494f7f66bc
xjtx <73387366+xiaoji235@users.noreply.github.com>
2023-02-04 13:27:07 +0800

    Create notice.js

---
+++ other/ctwp/js/notice.js
@@ -0,0 +1,12 @@
+mdui.snackbar({
+    message: '链接BUG已修复',
+    position: 'right-top',
+}); 
+<!--通知弹窗-- >
+mdui.dialog({
+    title: '通知',
+    content: '部分链接由于需要登录无法解析',
+    buttons: [{
+        text: '我知道了'
+    }]
+});
